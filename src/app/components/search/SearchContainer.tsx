5faa86bc208dd46741e94b80ae5ef48598cdf9ec
Joel Acef <joelacef@gmail.com>
2024-08-22 17:26:01 -0300

    delete styles

--- src/app/components/search/SearchContainer.tsx
+++ src/app/components/search/SearchContainer.tsx
@@ -43,32 +43,7 @@ const SearchContainer = () => {
                 anchorEl={anchorEl}
                 open={open}
                 onClose={handleClose}
-                PaperProps={{
-                    elevation: 2,
-                    sx: {
-                        overflow: 'visible',
-                        filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-                        mt: 1.5,
-                        '& .MuiAvatar-root': {
-                            width: 32,
-                            height: 32,
-                            ml: -0.5,
-                            mr: 1,
-                        },
-                        '&::before': {
-                            content: '""',
-                            display: 'block',
-                            position: 'absolute',
-                            top: 0,
-                            right: 40,
-                            width: 10,
-                            height: 10,
-                            bgcolor: 'background.paper',
-                            transform: 'translateY(-50%) rotate(45deg)',
-                            zIndex: 0,
-                        },
-                    },
-                }}
+                PaperProps={{ elevation: 2 }}
             >  
                 <Filter />
             </Menu>
